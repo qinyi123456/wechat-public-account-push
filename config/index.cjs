a88ebb086de5ca55716df34717a9de498566f2f6
qinyi123456 <120149231+qinyi123456@users.noreply.github.com>
2023-01-18 22:49:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '婷宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'od0pw57HcTVqYAL10XJF91hAsATQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -53,7 +53,7 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '熠熠',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'od0pw57vwSKdJkY2V3CFSGoYJdvE',
     }
