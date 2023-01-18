bdaac61e1d183c176ca8dde0d7c6f7387901060e
qinyi123456 <120149231+qinyi123456@users.noreply.github.com>
2023-01-18 11:47:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,11 +21,12 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'od0pw57HcTVqYAL10XJF91hAsATQ',
+      id：'od0pw57HcTVqYAL10XJF91hAsATQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '	qI_geESkgrLReBVWmoBqTIqf6Ke4N4PNex_T98jfhYc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '09-23',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
