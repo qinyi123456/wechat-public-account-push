be365f09449a733d9416d4d5030485ebde0129e2
qinyi123456 <120149231+qinyi123456@users.noreply.github.com>
2023-01-18 23:01:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '熠宝贝', year: '2000', date: '08-21',
+          type: '*生日', name: '熠宝贝', year: '2000', date: '08-21',
         },
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '11-12',
