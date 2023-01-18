2ed2aef1f09b70e865fb3cccb6c4930b97aef7bd
qinyi123456 <120149231+qinyi123456@users.noreply.github.com>
2023-01-18 22:41:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'od0pw57HcTVqYAL10XJF91hAsATQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	qI_geESkgrLReBVWmoBqTIqf6Ke4N4PNex_T98jfhYc',
+      useTemplateId: 'RRWg9Mhn1Q5nYdHmMSvYD2PRZOgbvNHGdIZkAinutYo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-23',
       festivals: [
@@ -49,7 +49,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'zvF0FBdtu6qGFWiQ-clFY7NtDsZXebk4kfqvGKY8A_M',
 
   CALLBACK_USERS: [
     {
